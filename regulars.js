799e59eac8f7994bb4d95cd6631a85df31651844
Dmitry <115485475+Nephilim1985@users.noreply.github.com>
2022-11-14 12:53:14 +0200

    Update regulars.js

--- regulars.js
+++ regulars.js
@@ -1,8 +1,35 @@
-let phoneValidator = function (phoneNumber) {
+/*Creating some function-validators for email, phone number and password using regular expressions*/
+
+
+/**
+ * Validator for correct email input
+ * @param {*} email checking email 
+ * @returns true or false depends on validate test result
+ */
+let emailValidator = function (email) {
     let validateRule = /^[A-Za-z\d][-+.A-Za-z\d]{1,19}@[.!$%&’*+/=?^\w-]{1,15}\.[a-zA-Z]{1,5}$/;
-    return phoneNumber.match(validateRule) !== null;
-
-
+    return email.match(validateRule) !== null;
 }
 
-console.log(phoneValidator("first.part@se=cond%part.r"))
+/**
+ * Validator for correct email input
+ * @param {*} phoneNumber checking phoneNumber 
+ * @returns true or false depends on validate test result
+ */
+let phoneValidator = function (phoneNumber) {
+    if (phoneNumber.lenght > 25) {
+        return false
+    }
+    let validateRule = /^[-\s]*(\+[-\s]*3[-\s]*8)?[-\s]*\(?0[-\s]*\d[-\s]*\d\)?([-\s]*\d){7}-*\s*$/;
+    return phoneNumber.match(validateRule) !== null;
+}
+
+/**
+ * Validator for correct email input
+ * @param {*} password checking password 
+ * @returns true or false depends on validate test result
+ */
+let passValidator = function (password) {
+    let validateRule = /(?=.*[A-Z])(?=.*[a-z])(?=.*\d)/;
+    return password.match(validateRule) !== null;
+}
